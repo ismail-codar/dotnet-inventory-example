a921a8116e1477752b57471e0105146eccf0a96b
İsmail Codar <icodar@hotmail.com>
2022-09-28 21:52:37 +0300

    s.s.s

--- App_Data/attach_db.sh
+++
@@ -1,4 +0,0 @@
-sleep 15s
- 
-/opt/mssql-tools/bin/sqlcmd -S . -U sa -P codaricodar!%2300CODARyekbas \
--Q "CREATE DATABASE Northwind ON (FILENAME = '/posdb/NORTHWND.MDF'),(FILENAME = '/posdb/NORTHWND_log.ldf') FOR ATTACH"
