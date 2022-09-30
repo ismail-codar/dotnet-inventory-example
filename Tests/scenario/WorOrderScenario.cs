27b84a2c3ebeb630130ad58ea2dfa4f90d6fc26c
İsmail Codar <icodar@hotmail.com>
2022-09-30 21:45:58 +0300

    ....

--- Tests/scenario/WorOrderScenario.cs
+++ Tests/scenario/WorOrderScenario.cs
@@ -26,14 +26,57 @@ internal class WorOrderScenario
         //     .ToList();
         // Console.WriteLine(list.Count);
 
-        WorkOrderService workOrderService = new WorkOrderService(this.options);
-        await workOrderService.Insert(new WorkOrder()
+        IProductService2 productService = new Product2Service(this.options);
+        IProductUnitService productUnitService = new ProductUnitService(this.options);
+        IStockBuildingService stockBuildingService = new StockBuildingService(this.options);
+        IStockRoomService stockRoomService = new StockRoomService(this.options);
+        IProductStockService productStockService = new ProductStockService(this.options);
+        IWorkOrderService workOrderService = new WorkOrderService(this.options);
+
+        Log.Debug("Test ProductUnit (paket) oluştur");
+        var productUnit = new ProductUnit()
         {
-            Date = DateTime.Now,
-            WorkOrderId = 0,
-            ProductId = 0,
-            SourceRoomId = null,
-            TargetRoomId = 0
-        });
+            UnitName = "Paket"
+        };
+        await productUnitService.Insert(productUnit);
+
+        Log.Debug("Test product oluştur");
+        var product = new Product2()
+        {
+            Description = "deneme",
+            ProductName = "Test product",
+            UnitId = productUnit.UnitId,
+            UnitPrice = 3,
+            UnitsInStock = 6
+        };
+        await productService.Insert(product);
+
+        Log.Debug("Stock building oluştur");
+        var stockBuilding = new StockBuilding()
+        {
+            BuildingName = "Bina 1",
+        };
+        await stockBuildingService.Insert(stockBuilding);
+
+
+        Log.Debug("Stock room oluştur");
+        var stockRoom = new StockRoom()
+        {
+            StockBuildingId = stockBuilding.StockBuildingId,
+            RoomName = "Oda 1"
+        };
+        await stockRoomService.Insert(stockRoom);
+
+        // TODO workOrder & productStock
+
+        // WorkOrderService workOrderService = new WorkOrderService(this.options);
+        // await workOrderService.Insert(new WorkOrder()
+        // {
+        //     Date = DateTime.Now,
+        //     WorkOrderId = 0,
+        //     ProductId = 0,
+        //     SourceRoomId = null,
+        //     TargetRoomId = 0
+        // });
     }
 }
